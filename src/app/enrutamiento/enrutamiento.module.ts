0686a48721b20e2faa54acfde1e714e7656ba12d
t1nch0 <t1ncht0t3@hotmail.com>
2019-07-23 20:10:24 -0400

    creando modulo y componenetes

---
+++ src/app/enrutamiento/enrutamiento.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { EnrutamientoRoutingModule } from './enrutamiento-routing.module';
+import { StudentComponent } from './student/student.component';
+import { StudentListComponent } from './student-list/student-list.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+
+
+@NgModule({
+  declarations: [StudentComponent, StudentListComponent, StudentDetailComponent],
+  imports: [
+    CommonModule,
+    EnrutamientoRoutingModule,
+    StudentComponent,
+    StudentDetailComponent,
+    StudentListComponent
+  ]
+})
+export class EnrutamientoModule { }
