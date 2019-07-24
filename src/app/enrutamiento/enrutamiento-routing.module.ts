1f1c3b520f1f49e8196930920e823c143fafbeb7
t1nch0 <t1ncht0t3@hotmail.com>
2019-07-23 20:31:56 -0400

    añadiendo a app.module y nuevos path

--- src/app/enrutamiento/enrutamiento-routing.module.ts
+++ src/app/enrutamiento/enrutamiento-routing.module.ts
@@ -1,10 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StudentComponent } from './student/student.component';
+import { StudentDetailComponent } from './student-detail/student-detail.component';
+import { StudentListComponent } from './student-list/student-list.component';
 
 
 const routes: Routes = [
-  { path: 'main', loadChildren: './features/main/main.module#MainModule' },
-  { path: '', pathMatch: 'full', redirectTo: 'main' }
+  { path: 'enrutamiento', loadChildren: './enrutamiento/enrutamiento.module#EnrutamientoModule' },
+  // { path: '', pathMatch: 'full', redirectTo: 'main' }
+  {
+    path: 'student',
+    component: StudentComponent,
+    children: [
+      {
+        path: 'list',
+        component: StudentListComponent
+      },
+      {
+        path: 'detail',
+        component: StudentDetailComponent
+      }
+
+    ]
+  }
 ];
 
 @NgModule({
