d2757deae991595d8794ca0a67192fa8656c9651
t1nch0 <t1ncht0t3@hotmail.com>
2019-07-23 20:18:56 -0400

    añadiendo routes

--- src/app/enrutamiento/enrutamiento-routing.module.ts
+++ src/app/enrutamiento/enrutamiento-routing.module.ts
@@ -2,7 +2,10 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'main', loadChildren: './features/main/main.module#MainModule' },
+  { path: '', pathMatch: 'full', redirectTo: 'main' }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
